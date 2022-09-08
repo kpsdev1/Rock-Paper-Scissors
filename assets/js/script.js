0dee410fe7ee4efaace11b70baee060b981916e4
kpsdev1 <stuart151@hotmail.com>
2022-09-08 16:05:15 +0000

    Creating js variables and storing them inside an event listener

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+let computerSelections = ['rock', 'paper', 'scissors'];
+let computerHand;
+let userHand;
+let playerChoice = document.getElementById('playerchoice');
+let computerChoice = document.getElementById('computerchoice');
+let roundResult = document.getElementById('result');
+let userImage =  document.getElementById('userpic');
+let userResult = document.getElementById('user-result');
+let computerResult = document.getElementById('computer-result');
+let computerImage =  document.getElementById('comppic');
+let messageDiv = document.getElementById('winnerMessage');
+let playerScore = 0;
+let computerScore = 0;
+let resetBtn = messageDiv.querySelector('button');
+
+
+
+
+})
